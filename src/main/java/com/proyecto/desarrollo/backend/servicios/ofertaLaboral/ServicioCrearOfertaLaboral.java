e1757867ba5c839b3584ee459f5f6a5dd31216e5
robegon98 <ragonzalez1899@gmail.com>
2021-06-29 13:37:50 -0400

    Crear vista de creación de oferta laboral
    Se creó la vista de creacion de oferta laboral, incluyendo validadores en los campos de entrada. Esta vista crea una instancia de Oferta Laboral que todavía no se ha convertido en un DTO ya que no se han implementados los métodos para la conexión con el Back.
    
    Se creo su propia hoja de estilos y su servicio que actualmente no hace la consulta con el back para obtener las empresas registradas en el sistema.

---
+++ src/main/java/com/proyecto/desarrollo/backend/servicios/ofertaLaboral/ServicioCrearOfertaLaboral.java
@@ -0,0 +1,79 @@
+package com.proyecto.desarrollo.backend.servicios.ofertaLaboral;
+
+import com.proyecto.desarrollo.backend.entidades.empresas.infraestructura.DTO.entrada.ConsultarEmpresasParaCreacion;
+import com.proyecto.desarrollo.backend.entidades.ofertaLaboral.dominio.OfertaLaboral;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Div;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioCrearOfertaLaboral {
+    private ConsultarEmpresasParaCreacion[] empresas;
+    private int cont;
+
+    public ConsultarEmpresasParaCreacion[] obtenerEmpresas(){
+
+        return empresas;
+    }
+
+    public OfertaLaboral crearOferta(String titulo, String descripcion, String cargo, float sueldo, int valor, String escala, String turno, int vacantes, String empresa){
+        return new OfertaLaboral(titulo,descripcion,cargo,sueldo,valor,escala,turno,vacantes,empresa);
+    }
+
+
+    public boolean ofertaValida(OfertaLaboral ofertaCreada) {
+        Dialog modal = new Dialog();
+        if (ofertaCreada.getTitulo().getTitulo() == "invalido"){
+
+            modal.add(new Div(new Text("El titulo debe contener de 4 a 80 caracteres")), new Button("Cerrar", event -> modal.close()));
+            modal.open();
+            return false;
+        }
+
+        else if (ofertaCreada.getDescripcion().getDescripcion() == "invalido"){
+            modal.add(new Div(new Text("La descripcion debe contener de 32 a 512 caracteres")), new Button("Cerrar", event -> modal.close()));
+            modal.open();
+        return false;
+        }
+
+        else if (ofertaCreada.getCargo().getCargo() == "invalido"){
+            modal.add(new Div(new Text("El cargo debe contener de 4 a 40 caracteres")), new Button("Cerrar", event -> modal.close()));
+            modal.open();
+            return false;
+        }
+
+        else if (ofertaCreada.getDuracionEstimadaEscala().getEscala() == "invalido"){
+            modal.add(new Div(new Text("La duracion debe ser horas, dias, semanas o meses")), new Button("Cerrar", event -> modal.close()));
+            modal.open();
+        return false;
+        }
+
+        else if (ofertaCreada.getTurnoTrabajo().getTurno() == "invalido"){
+            modal.add(new Div(new Text("Los turnos pueden ser diurno, nocturno o mixto")), new Button("Cerrar", event -> modal.close()));
+            modal.open();
+            return false;
+        }
+
+        else if(ofertaCreada.getSueldo().getSueldo() == -1){
+            modal.add(new Div(new Text("El sueldo debe ser mayor a 0 pero menor a 1.000.000")), new Button("Cerrar", event -> modal.close()));
+            modal.open();
+        return false;
+    }
+
+        else if (ofertaCreada.getDuracionEstimadaValor().getValor() == -1){
+            modal.add(new Div(new Text("La duracion puede ser desde 0 a 99 (horas, dias, meses, años)")), new Button("Cerrar", event -> modal.close()));
+            modal.open();
+            return false;
+        }
+
+        else if (ofertaCreada.getNumeroVacantes().getVacantes() == -1) {
+            modal.add(new Div(new Text("Los puestos disponibles pueden ser desde 0 a 99")), new Button("Cerrar", event -> modal.close()));
+            modal.open();
+            return false;
+        }
+
+        return true;
+    }
+}
--- src/main/java/com/proyecto/desarrollo/ui/vistas/ofertasTrabajo/CrearOfertaLaboral_vista.java
+++ src/main/java/com/proyecto/desarrollo/ui/vistas/ofertasTrabajo/CrearOfertaLaboral_vista.java
@@ -1,5 +1,200 @@
 package com.proyecto.desarrollo.ui.vistas.ofertasTrabajo;
 
-public class CrearOfertaLaboral_vista {
+import com.proyecto.desarrollo.backend.entidades.empresas.infraestructura.DTO.entrada.ConsultarEmpresasParaCreacion;
+import com.proyecto.desarrollo.backend.entidades.ofertaLaboral.dominio.OfertaLaboral;
+import com.proyecto.desarrollo.backend.servicios.ofertaLaboral.ServicioCrearOfertaLaboral;
+import com.proyecto.desarrollo.ui.vistas.layout.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+
+
+@Route(value = "crear-ofertas-laboral", layout = MainLayout.class)
+@PageTitle("Crear Ofertas Laboral")
+
+public class CrearOfertaLaboral_vista extends Div{
+
+
+    private TextField titulo;
+
+    private TextArea descripcion;
+
+    private TextField cargo;
+
+    private TextField sueldo;
+
+    private TextField duracion;
+
+    private Select<String> escala;
+
+    private Select<String> turno;
+
+    private TextField vacantes;
+
+    private Select<ConsultarEmpresasParaCreacion> empresas;
+
+    private Button submitt;
+
+    private Button cancelar;
+
+    private ServicioCrearOfertaLaboral controlador;
+
+    public CrearOfertaLaboral_vista(){
+        controlador = new ServicioCrearOfertaLaboral();
+
+        setHeightFull();
+        addClassName("contenido");
+
+        VerticalLayout contenedor = new VerticalLayout();
+        contenedor.setClassName("space");
+
+        Div card = new Div();
+        card.setClassName("card form");
+
+        HorizontalLayout encabezado = new HorizontalLayout();
+        encabezado.setClassName("encabezado");
+        encabezado.add(new H3("Crear Oferta Laboral"));
+        encabezado.setWidthFull();
+
+
+        Div contenido = new Div();
+
+
+        FormLayout crearOferta = new FormLayout();
+        crearOferta.setMaxWidth("80em");
+
+        titulo = new TextField();
+        titulo.setMaxLength(16);
+        titulo.setHelperText("Maximo 80 caracteres");
+        titulo.addValueChangeListener(e-> {
+            if (listoParaEnviar() && !sueldo.isInvalid() && !duracion.isInvalid() && !vacantes.isInvalid()) submitt.setEnabled(true);
+        });
+        titulo.setLabel("Titulo");
+        titulo.setPlaceholder("Se busca Conserje");
+
+        descripcion = new TextArea();
+        descripcion.setMaxLength(512);
+        descripcion.setHelperText("Maximo 512 caracteres");
+        descripcion.addValueChangeListener(e-> {
+            if (listoParaEnviar() && !sueldo.isInvalid() && !duracion.isInvalid() && !vacantes.isInvalid()) submitt.setEnabled(true);
+        });
+        descripcion.setLabel("Descripción");
+        descripcion.setPlaceholder("Se solicita personal de limpieza para la empresa Orangesoft");
+
+        cargo = new TextField();
+        cargo.setMaxLength(40);
+        cargo.setHelperText("Maximo 40 caracteres");
+        cargo.addValueChangeListener(e-> {
+            if (listoParaEnviar() && !sueldo.isInvalid() && !duracion.isInvalid() && !vacantes.isInvalid()) submitt.setEnabled(true);
+        });
+        cargo.setLabel("Cargo");
+        cargo.setPlaceholder("Conserje");
+
+
+        sueldo = new TextField();
+        sueldo.setPattern("^([0-9]*[.])?[0-9]{2}$");
+        sueldo.setMaxLength(9);
+        sueldo.setHelperText("Maximo 2 decimales y 1000000.   Ej: 5012.00");
+        sueldo.addValueChangeListener(e-> {
+            if (listoParaEnviar() && !sueldo.isInvalid() && !duracion.isInvalid() && !vacantes.isInvalid()) submitt.setEnabled(true);
+            System.out.println(!sueldo.isInvalid());
+        });
+        sueldo.setLabel("Sueldo($)");
+        sueldo.setPlaceholder("800");
+
+        duracion = new TextField();
+        duracion.setMaxLength(2);
+        duracion.setHelperText("Maximo 99");
+        duracion.addValueChangeListener(e-> {
+            if (listoParaEnviar() && !sueldo.isInvalid() && !duracion.isInvalid() && !vacantes.isInvalid()) submitt.setEnabled(true);
+        });
+        duracion.setPattern("[1-9]?[0-9]");
+        duracion.setLabel("Duracion");
+        duracion.setPlaceholder("5");
+
+        escala = new Select<>();
+        escala.addValueChangeListener(e-> {
+            if (listoParaEnviar() && !sueldo.isInvalid() && !duracion.isInvalid() && !vacantes.isInvalid()) submitt.setEnabled(true);
+        });
+        escala.setItems("hora","dia","mes","año");
+
+        turno = new Select<>();
+        turno.addValueChangeListener(e-> {
+            if (listoParaEnviar() && !sueldo.isInvalid() && !duracion.isInvalid() && !vacantes.isInvalid()) submitt.setEnabled(true);
+        });
+        turno.setItems("diurno","nocturno","mixto");
+        turno.setLabel("Turno de trabajo");
+        turno.setWidth("3em");
+
+        vacantes = new TextField();
+        vacantes.setMaxLength(2);
+        vacantes.setHelperText("Maximo 99");
+        vacantes.addValueChangeListener(e-> {
+            if (listoParaEnviar() && !sueldo.isInvalid() && !duracion.isInvalid() && !vacantes.isInvalid()) submitt.setEnabled(true);
+        });
+        vacantes.setLabel("Cantidad de puestos disponibles");
+        vacantes.setPattern("[1-9]?[0-9]");
+        vacantes.setPlaceholder("3");
+
+        /*Falta el ID de la empresa*/
+        crearOferta.add(titulo, cargo,sueldo,duracion,escala,turno,vacantes,descripcion);
+
+        contenido.add(crearOferta);
+        contenido.setClassName("espacio");
+
+
+        Div enviar = new Div();
+        enviar.setWidthFull();
+        Div inner = new Div();
+        inner.setWidth("250px");
+        inner.setClassName("enviar");
+        submitt = new Button("Crear");
+        submitt.setClassName("btonEnviar");
+        submitt.addClickListener(e -> crear());
+        submitt.setEnabled(false);
+        cancelar = new Button("Cancelar");
+
+        cancelar.setClassName("cancelar buttonsSpace");
+        RouterLink routerLink = new RouterLink("",OfertasTrabajo_vista.class);
+        routerLink.getElement().appendChild(cancelar.getElement());
+        inner.add(routerLink,submitt);
+        enviar.add(inner);
+
+
+        card.add(encabezado,contenido,enviar);
+        contenedor.add(card);
+
+        crearOferta.setResponsiveSteps(
+                new FormLayout.ResponsiveStep("1px",1),
+                new FormLayout.ResponsiveStep("600px",2),
+                new FormLayout.ResponsiveStep("700px",3)
+        );
+
+        add(contenedor);
+    }
+
+    private void crear() {
+
+        OfertaLaboral ofertaCreada = controlador.crearOferta(this.titulo.getValue(),this.descripcion.getValue(),this.cargo.getValue(),Float.parseFloat(this.sueldo.getValue()),Integer.parseInt(this.duracion.getValue()),this.escala.getValue(),this.turno.getValue(), Integer.parseInt(this.vacantes.getValue()),"1");
+        System.out.println(ofertaCreada.getTitulo().getTitulo());
+        if(controlador.ofertaValida(ofertaCreada))
+            this.submitt.getUI().ifPresent(ui ->ui.navigate(OfertasTrabajo_vista.class));
+    }
+
+    private boolean listoParaEnviar(){
+        if (this.titulo.isEmpty() || this.descripcion.isEmpty() || this.cargo.isEmpty() || this.sueldo.isEmpty() || this.duracion.isEmpty() || this.escala.isEmpty() || this.turno.isEmpty() || this.vacantes.isEmpty())
+            return false;
+        return true;
+    }
+
 
 }
