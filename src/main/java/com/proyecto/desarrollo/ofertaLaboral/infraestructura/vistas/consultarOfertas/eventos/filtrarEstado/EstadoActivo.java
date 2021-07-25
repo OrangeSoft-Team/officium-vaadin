cad89e13f9e23380817727ce68f3b3b1db1193ea
robegon98 <ragonzalez1899@gmail.com>
2021-07-24 21:38:24 -0400

    Modifica Jsons
    Se modificaron los jsons debido a que tenian problemas en algunos campos, estos eran Strings, pero en verdad eran float e int.

--- src/main/java/com/proyecto/desarrollo/ofertaLaboral/infraestructura/vistas/consultarOfertas/eventos/filtrarEstado/EstadoActivo.java
+++ src/main/java/com/proyecto/desarrollo/ofertaLaboral/infraestructura/vistas/consultarOfertas/eventos/filtrarEstado/EstadoActivo.java
@@ -1,4 +1,4 @@
-package com.proyecto.desarrollo.ofertaLaboral.infraestructura.vistas.consultarOfertas.eventos;
+package com.proyecto.desarrollo.ofertaLaboral.infraestructura.vistas.consultarOfertas.eventos.filtrarEstado;
 
 import com.proyecto.desarrollo.ofertaLaboral.infraestructura.vistas.consultarOfertas.OfertasTrabajo_vista;
 
