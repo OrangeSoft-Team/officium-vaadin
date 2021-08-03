124b5baa07af4c6b11dea07144918914b6123306
Daniel Bello <dbcode.18@gmail.com>
2021-08-03 17:52:40 -0400

    Implementa metodo obtener_lista_empleados del adptador de p de empleados
    Se implemento el metodo obtener_lista_empleados del adaptador de persistencia de empleados (Nest). Ahora retorna los datos reales del endpoint. CU 9.1 y 9.2

--- src/main/java/com/proyecto/desarrollo/empleados/aplicacion/persistencia/PersistenciaEmpleado.java
+++ src/main/java/com/proyecto/desarrollo/empleados/aplicacion/persistencia/PersistenciaEmpleado.java
@@ -3,13 +3,14 @@ package com.proyecto.desarrollo.empleados.aplicacion.persistencia;
 import com.proyecto.desarrollo.empleados.infraestructura.DTO.entrada.EmpleadoDetalleEntradaDTO;
 import com.proyecto.desarrollo.empleados.infraestructura.DTO.salida.EmpleadoDetalleSalidaDTO;
 import com.proyecto.desarrollo.empleados.infraestructura.DTO.entrada.EmpleadoEntradaDTO;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public interface PersistenciaEmpleado {
 
-    public ArrayList<EmpleadoEntradaDTO> obtener_lista_empleados() throws IOException;
+    public ArrayList<EmpleadoEntradaDTO> obtener_lista_empleados() throws IOException, ParseException;
     public EmpleadoDetalleEntradaDTO obtener_detalle_empleado(EmpleadoDetalleSalidaDTO credenciales) throws IOException;
 
 }
--- src/main/java/com/proyecto/desarrollo/empleados/infraestructura/persistencia/PersistenciaEmpleadoNest.java
+++ src/main/java/com/proyecto/desarrollo/empleados/infraestructura/persistencia/PersistenciaEmpleadoNest.java
@@ -4,18 +4,34 @@ import com.proyecto.desarrollo.empleados.aplicacion.persistencia.PersistenciaEmp
 import com.proyecto.desarrollo.empleados.infraestructura.DTO.entrada.EmpleadoDetalleEntradaDTO;
 import com.proyecto.desarrollo.empleados.infraestructura.DTO.entrada.EmpleadoEntradaDTO;
 import com.proyecto.desarrollo.empleados.infraestructura.DTO.salida.EmpleadoDetalleSalidaDTO;
+import com.proyecto.desarrollo.helpers.infraestructura.http.ManejadorHttp;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class PersistenciaEmpleadoNest implements PersistenciaEmpleado {
 
+    private String url_base = new String("https://officium-nest.herokuapp.com");
 
     @Override
-    public ArrayList<EmpleadoEntradaDTO> obtener_lista_empleados() throws IOException {
+    public ArrayList<EmpleadoEntradaDTO> obtener_lista_empleados() throws IOException, ParseException {
         ArrayList<EmpleadoEntradaDTO> lista_empleados = new ArrayList<EmpleadoEntradaDTO>();
-        lista_empleados.add(new EmpleadoEntradaDTO("444" , "Juan" , "Roberto" , "correo@correo" , "activo" , "mascuino"));
-        lista_empleados.add(new EmpleadoEntradaDTO("222" , "Luis" , "Salazar" , "correo22@correo" , "activo" , "mascuino"));
+
+        URL url = new URL(this.url_base + "/api/staff/empleados");
+        String respuesta_string = ManejadorHttp.realizar_peticion_get(url);
+
+        JSONParser parser = new JSONParser();
+        JSONArray json = (JSONArray) parser.parse(respuesta_string);
+        json.forEach(item -> {
+            JSONObject item_jsonobj = (JSONObject) item;
+            lista_empleados.add(new EmpleadoEntradaDTO(item_jsonobj.get("uuid").toString() , item_jsonobj.get("primerNombre").toString() , item_jsonobj.get("primerApellido").toString() , item_jsonobj.get("correo").toString() , item_jsonobj.get("estatus").toString() , item_jsonobj.get("genero").toString()));
+        });
+
         return lista_empleados;
     }
 
